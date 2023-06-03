a9362533ea9654d83cb9606e5bbaeef3fe63c383
Ilya Safronov <IlyaSafronov18@gmail.com>
2023-06-04 01:01:20 +0300

    js step one

---
+++ js/script.js
@@ -0,0 +1,48 @@
+'use strict';
+const musicContainer = document.querySelector('.music-container');
+const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
+const title = document.querySelector('#title');
+const cover = document.querySelector('#cover');
+
+// ! Songs titles 
+const songs = ['hey', 'summer', 'ukulele'];
+
+// ! Keep track of the songs 
+let songIndex = 2;
+
+// ! Initially load song info DOM 
+loadSong(songs[songIndex]);
+
+// ! Update song details 
+function loadSong(song) {
+  title.innerText = song
+  audio.src = `music/${song}.mp3`
+  cover.src = `images/${song}.jpg`
+}
+
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+}
+
+function pauseSong() {
+
+}
+
+// ! Event listeners
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if(isPlaying) {
+    pauseSong()
+  } else {
+    playSong()
+  }
+})
