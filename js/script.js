bd4d9137e2c37f4b2979f057734ab56813b9aa4e
Ilya Safronov <IlyaSafronov18@gmail.com>
2023-06-06 15:56:50 +0300

    js continue

--- js/script.js
+++ js/script.js
@@ -8,21 +8,23 @@ const progress = document.querySelector('.progress');
 const progressContainer = document.querySelector('.progress-container');
 const title = document.querySelector('#title');
 const cover = document.querySelector('#cover');
+const currentTime = document.querySelector('#currTime');
+const durTime = document.querySelector('#durTime');
 
-// ! Songs titles 
+// ! Song titles 
 const songs = ['hey', 'summer', 'ukulele'];
 
-// ! Keep track of the songs 
+// ! Keep track of the song
 let songIndex = 2;
 
-// ! Initially load song info DOM 
+// ! Initially load song into DOM 
 loadSong(songs[songIndex]);
 
 // ! Update song details 
 function loadSong(song) {
-  title.innerText = song
-  audio.src = `music/${song}.mp3`
-  cover.src = `images/${song}.jpg`
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
 }
 
 // ! Play song
@@ -49,45 +51,105 @@ function pauseSong() {
 // ! Previos song
 
 function prevSong() {
-  songIndex--
+  songIndex--;
 
   if(songIndex < 0) {
-    songIndex = songs.length - 1
+    songIndex = songs.length - 1;
   }
-  loadSong(songs[songIndex])
-  playSong()
+  loadSong(songs[songIndex]);
+
+  playSong();
 }
 
 // ! Next song
 
 function nextSong() {
-  songIndex++
+  songIndex++;
 
   if(songIndex > songs.length - 1) {
-    songIndex = 0
+    songIndex = 0;
   }
-  loadSong(songs[songIndex])
+  loadSong(songs[songIndex]);
 
-  playSong()
+  playSong();
 }
 
 // ! Update progress bar
 
 function updateProgress(e) {
-  const { duration, currentTime } = e.srcElement
-  const progressPercent = (currentTime / duration) * 100
-  progress.style.width = `${progressPercent}%`
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
 }
 
 // ! Set progress bar
 
 function setProgress(e) {
-  const width = this.clientWidth
-  const clickX = e.offsetX
-  const duration = audio.duration
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
 
-  audio.currentTime = (clickX / width) * duration
+  audio.currentTime = (clickX / width) * duration;
 }
+
+// ! Get duration & currentTime for Time of song
+
+function DurTime (e) {
+  const { duration, currentTime } = e.srcElement;
+  var sec;
+  var sec_d;
+
+  // todo define seconds currentTime 
+  let min = (currentTime == null)? 0:
+  Math.floor(currentTime / 60);
+  min = min < 10 ? '0' +min:min;
+
+  // todo define seconds currentTime
+
+function get_sec (x) {
+  if(Math.floor(x) >= 60) {
+    for(var i = 1; i <= 60; i++) {
+      if(Math.floor(x) >= (60 * i) && Math.floor(x) < (60 * (i + 1))) {
+        sec = Math.floor(x) - (60 * i);
+        sec = sec < 10 ? '0' + sec:sec;
+      }
+    }
+  } else { 
+    sec = Math.floor(x);
+    sec = sec < 10 ? '0' +sec:sec;
+  }
+}
+
+get_sec (currentTime, sec);
+// todo change currentTime DOM 
+currentTime.innerHTML = min +':'+ sec;
+
+// todo difine minutes duration 
+let min_d = (isNaN(duration) === true)? '0':
+Math.floor(duration / 60);
+min_d = min_d < 10 ? '0'+min_d:min_d;
+
+function get_sec_d (x) {
+  if(Math.floor(x) >= 60) {
+    for(var i = 1; i <= 60; i++) {
+      if(Math.floor(x) >= (60 * i) && Math.floor(x) < (60 * (i + 1))) {
+        sec_d = Math.floor(x) - (60 * i);
+        sec_d = sec_d < 10 ? '0'+sec_d:sec_d;
+      }
+    }
+  } else {
+    sec_d = (isNaN(duration) === true)? '0':
+    Math.floor(x);
+    sec_d = sec_d < 10 ? '0'+sec_d:sec_d;
+  }
+}
+
+// todo define seconds duration 
+get_sec_d (duration);
+
+// todo change duration DOM 
+durTime.innerHTML = min_d + ':'+ sec_d;
+};
 
 // ! Event listeners
 playBtn.addEventListener('click', () => {
@@ -104,8 +166,14 @@ playBtn.addEventListener('click', () => {
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 
+// ! Time/song update
 audio.addEventListener('timeupdate', updateProgress);
 
+// ! Click on progress bar
 progressContainer.addEventListener('click', setProgress);
 
+// ! Song ends
 audio.addEventListener('ended', nextSong);
+
+// ! Time of song 
+audio.addEventListener('timeupdate', DurTime);
